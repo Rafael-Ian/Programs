fa05938003eb326c98e5e760220374425f36d6cd
Rafael-Ian <cabertesilorio@gmai.com>
2025-03-07 18:29:59 +0800

    Prog2

--- Programs.py
+++ Programs.py
@@ -9,7 +9,15 @@ elif num2 > num1:
 else:
     print("The numbers are equal.")
 #Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+if num1 == num2:
+    print('Equal')
+else:
+    print("Not Equal")
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
 #Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
 #Prog05: Create a program that ask user to input 2 numbers. Print the quotient of the two numbers with the decimal point.
 #Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
@@ -17,4 +25,3 @@ else:
 #Prog08: Create a program that ask user to input 10 numbers. Print how many are odd numbers.
 #Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 #Prog10: Create a program that print all the numbers starting from 0 to 100 except numbers ending in zero.
-
